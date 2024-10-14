192ff8dd2d83202cd22f99ba8cf2d7ff8bd33efc
VincentGaligaro <jhangvincent19@gmail.com>
2024-10-14 21:15:56 +0800

    app.js

---
+++ app.js
@@ -0,0 +1,14 @@
+const car = {
+    type: "Sedan",
+    model: "Camry",
+    color: "Blue"
+};
+
+
+console.log(typeof car);
+
+car.type = "Toyota";
+console.log(car);
+
+car.wheels = 4;
+console.log(car);
